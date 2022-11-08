72767587094c1af1492d31ebed3f9a786ca1151c
enriquea <enrique.audain@gmail.com>
2022-11-08 14:23:13 +0100

    updated

--- bcl-convert-installer/install.sh
+++ bcl-convert-installer/install.sh
@@ -10,7 +10,8 @@ set -e
 set -u
 set -o pipefail
 
-# Description here
+# Simple script to convert and install the rpm package bcl-convert
+# from Illumina on Ubuntu 22.04.
 
 sudo apt-get update
 sudo apt-get upgrade
@@ -26,10 +27,11 @@ wget -c https://webdata.illumina.com/downloads/software/bcl-convert/bcl-convert-
 # Convert rpm pkg to debian
 sudo alien -d bcl-convert-4.0.3-2.el7.x86_64.rpm
 
-# Install pkg (default installation dir -> /usr/bin)
-sudo alien -i bcl-convert-4.0.3-2.el7.x86_64.deb
+# Install pkg (default installation dir -> /usr/bin/)
+sudo dpkg -i bcl-convert_4.0.3*.deb 
 
-rm ./*.deb
+# complete installation and required dependences
+# sudo apt-get -f install
 
 echo "Completed!"
 
