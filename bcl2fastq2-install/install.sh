09533dac4dbf29a924f6793b800394a469bdd0d7
enriquea <enrique.audain@gmail.com>
2022-11-09 00:58:22 +0100

    updated

--- bcl2fastq2-install/install.sh
+++ bcl2fastq2-install/install.sh
@@ -10,7 +10,7 @@ set -e
 set -u
 set -o pipefail
 
-# Simple script to convert and install the rpm package bcl-convert
+# Simple script to convert and install the rpm package bcl2fastq2
 # from Illumina on Ubuntu 22.04.
 
 sudo apt-get update
@@ -18,17 +18,14 @@ sudo apt-get upgrade
 
 sudo apt install alien
 
-mkdir -p ./pkg_tools
-cd pkg_tools
-
-# Download bcl-convert pkg
-wget -c https://webdata.illumina.com/downloads/software/bcl-convert/bcl-convert-4.0.3-2.el7.x86_64.rpm
+# unzip bcl2fastq2
+unzip ./pkg_rpm/bcl2fastq2-v2-20-0-linux-x86-64.zip
 
 # Convert rpm pkg to debian
-sudo alien -d bcl-convert-4.0.3-2.el7.x86_64.rpm
+sudo alien -d ./pkg_rpm/bcl2fastq2-v2.20.0.422-Linux-x86_64.rpm
 
 # Install pkg (default installation dir -> /usr/bin/)
-sudo dpkg -i bcl-convert_4.0.3*.deb
+sudo dpkg -i ./pkg_rpm/bcl2fastq2-v2.20.0*.deb
 
 # complete installation and required dependencies
 # sudo apt-get -f install
