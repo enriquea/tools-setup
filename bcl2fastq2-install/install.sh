7e7954cdc80215acd51b4138de90a15d2b7437fb
enriquea <enrique.audain@gmail.com>
2022-11-09 01:30:04 +0100

    minor

--- bcl2fastq2-install/install.sh
+++ bcl2fastq2-install/install.sh
@@ -19,13 +19,13 @@ sudo apt-get upgrade
 sudo apt install alien
 
 # unzip bcl2fastq2
-unzip ./pkg_rpm/bcl2fastq2-v2-20-0-linux-x86-64.zip
+sudo unzip ./pkg_rpm/bcl2fastq2-v2-20-0-linux-x86-64.zip -d ./pkg_rpm
 
 # Convert rpm pkg to debian
 sudo alien -d ./pkg_rpm/bcl2fastq2-v2.20.0.422-Linux-x86_64.rpm
 
 # Install pkg (default installation dir -> /usr/bin/)
-sudo dpkg -i ./pkg_rpm/bcl2fastq2-v2.20.0*.deb
+sudo dpkg -i bcl2fastq2_0v2.20.0.422*.deb
 
 # complete installation and required dependencies
 # sudo apt-get -f install
